f16b595b20758902ecf09759a804f398bb63aeed
Nouurchene <Nouurchenebenabdallah3@gmail.com>
2022-12-10 19:25:28 +0100

    nounou

---
+++ project_managment_LCS3-master/Migrations/202212081608186_nounoudb.cs
@@ -0,0 +1,146 @@
+﻿namespace project.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class nounoudb : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.projects",
+                c => new
+                    {
+                        projectId = c.Int(nullable: false, identity: true),
+                        name = c.String(nullable: false),
+                        description = c.String(nullable: false),
+                        endDate = c.DateTime(nullable: false),
+                        userId = c.String(maxLength: 128),
+                    })
+                .PrimaryKey(t => t.projectId)
+                .ForeignKey("dbo.AspNetUsers", t => t.userId)
+                .Index(t => t.userId);
+            
+            CreateTable(
+                "dbo.tasks",
+                c => new
+                    {
+                        taskId = c.Int(nullable: false, identity: true),
+                        name = c.String(),
+                        description = c.String(),
+                        startDate = c.DateTime(nullable: false),
+                        endDate = c.DateTime(nullable: false),
+                        workflow = c.String(),
+                        type = c.String(),
+                        projectId = c.Int(nullable: false),
+                        userId = c.String(maxLength: 128),
+                    })
+                .PrimaryKey(t => t.taskId)
+                .ForeignKey("dbo.projects", t => t.projectId, cascadeDelete: true)
+                .ForeignKey("dbo.AspNetUsers", t => t.userId)
+                .Index(t => t.projectId)
+                .Index(t => t.userId);
+            
+            CreateTable(
+                "dbo.AspNetUsers",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        FirstName = c.String(),
+                        LastName = c.String(),
+                        Gender = c.String(),
+                        DateOfBirthTime = c.DateTime(),
+                        hireDate = c.DateTime(),
+                        Email = c.String(maxLength: 256),
+                        EmailConfirmed = c.Boolean(nullable: false),
+                        PasswordHash = c.String(),
+                        SecurityStamp = c.String(),
+                        PhoneNumber = c.String(),
+                        PhoneNumberConfirmed = c.Boolean(nullable: false),
+                        TwoFactorEnabled = c.Boolean(nullable: false),
+                        LockoutEndDateUtc = c.DateTime(),
+                        LockoutEnabled = c.Boolean(nullable: false),
+                        AccessFailedCount = c.Int(nullable: false),
+                        UserName = c.String(nullable: false, maxLength: 256),
+                    })
+                .PrimaryKey(t => t.Id)
+                .Index(t => t.UserName, unique: true, name: "UserNameIndex");
+            
+            CreateTable(
+                "dbo.AspNetUserClaims",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        UserId = c.String(nullable: false, maxLength: 128),
+                        ClaimType = c.String(),
+                        ClaimValue = c.String(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
+                .Index(t => t.UserId);
+            
+            CreateTable(
+                "dbo.AspNetUserLogins",
+                c => new
+                    {
+                        LoginProvider = c.String(nullable: false, maxLength: 128),
+                        ProviderKey = c.String(nullable: false, maxLength: 128),
+                        UserId = c.String(nullable: false, maxLength: 128),
+                    })
+                .PrimaryKey(t => new { t.LoginProvider, t.ProviderKey, t.UserId })
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
+                .Index(t => t.UserId);
+            
+            CreateTable(
+                "dbo.AspNetUserRoles",
+                c => new
+                    {
+                        UserId = c.String(nullable: false, maxLength: 128),
+                        RoleId = c.String(nullable: false, maxLength: 128),
+                    })
+                .PrimaryKey(t => new { t.UserId, t.RoleId })
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
+                .ForeignKey("dbo.AspNetRoles", t => t.RoleId, cascadeDelete: true)
+                .Index(t => t.UserId)
+                .Index(t => t.RoleId);
+            
+            CreateTable(
+                "dbo.AspNetRoles",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Name = c.String(nullable: false, maxLength: 256),
+                    })
+                .PrimaryKey(t => t.Id)
+                .Index(t => t.Name, unique: true, name: "RoleNameIndex");
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.AspNetUserRoles", "RoleId", "dbo.AspNetRoles");
+            DropForeignKey("dbo.tasks", "userId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.AspNetUserRoles", "UserId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.projects", "userId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.AspNetUserLogins", "UserId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.AspNetUserClaims", "UserId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.tasks", "projectId", "dbo.projects");
+            DropIndex("dbo.AspNetRoles", "RoleNameIndex");
+            DropIndex("dbo.AspNetUserRoles", new[] { "RoleId" });
+            DropIndex("dbo.AspNetUserRoles", new[] { "UserId" });
+            DropIndex("dbo.AspNetUserLogins", new[] { "UserId" });
+            DropIndex("dbo.AspNetUserClaims", new[] { "UserId" });
+            DropIndex("dbo.AspNetUsers", "UserNameIndex");
+            DropIndex("dbo.tasks", new[] { "userId" });
+            DropIndex("dbo.tasks", new[] { "projectId" });
+            DropIndex("dbo.projects", new[] { "userId" });
+            DropTable("dbo.AspNetRoles");
+            DropTable("dbo.AspNetUserRoles");
+            DropTable("dbo.AspNetUserLogins");
+            DropTable("dbo.AspNetUserClaims");
+            DropTable("dbo.AspNetUsers");
+            DropTable("dbo.tasks");
+            DropTable("dbo.projects");
+        }
+    }
+}
